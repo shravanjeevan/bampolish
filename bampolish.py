de75950679519ca0baf8f3ae024bdd89257d4838
Kieran Walsh <k.walsh@student.unsw.edu.au>
2019-10-22 02:30:00 +1100

    Syntax fixes

--- bampolish.py
+++ bampolish.py
@@ -4,7 +4,7 @@ import argparse
 import pysam
 from sortedcontainers import SortedSet
 
-def main(argv):
+def main():
 
     parser = argparse.ArgumentParser(description="bampolish - A tool to normalise coverage in long read sequencing pipelines")
 
@@ -13,42 +13,49 @@ def main(argv):
     parser.add_argument("-o", "--output",
                         help="Output sam, bam or bed filename")
     parser.add_argument("-v", "--verbose", action='store_true',
-                        help="Verbose output")
+                        help="Verbose output", default=False)
     parser.add_argument("-w", "--window",
-                        help="Preffered window size to calculate average coverage for")
+                        help="Preffered window size to calculate average coverage for", default=1000)
     parser.add_argument("-c", "--coverage",
-                        help="Desired coverage limit for sequence for read")
+                        help="Desired coverage limit for sequence for read", default=10)
 
     args = parser.parse_args()
-    coverage(args.i, args.o, args.c)
+    coverage(args.input, args.output, args.coverage)
 
 #TODO obtain overall window average rather than strictly cut
-def coverage(inputBam, outputBam, maxCoverage = 10) {
+def coverage(inputBam, outputBam, maxCoverage):
     #Maintains a set of up to maxCoverage read end points
     curr = SortedSet() 
 
     #Iterate over mapped reads
-    bf = pysam.AlignemFile(inputBam, "rb")
-    for r in bf.fetch():
+    bf = pysam.AlignmentFile(inputBam, "rb")
+    
+    mapped = 0
+    filtered = 0
+    for r in bf.fetch(until_eof=True):
+        if(r.is_unmapped): continue
+        mapped += 1
+
         #Attempt to find a current read that ends before this read starts
-        itr = curr.irange(maximum=r.reference_start))
-        if(itr):
-            #Replace finished read with newly started read
-            curr.discard(itr.next())
+        itr = curr.irange(maximum=r.reference_start)
+
+        try:
+            ending = itr.__next__()
+            curr.discard(ending)
             curr.add(r.reference_end)
-            count += 1
+            filtered += 1
             
             #TODO Write this read to memory
 
-        else if len(curr) < maxCoverage:
+        except StopIteration:
             #Add read to attempt to meet coverage
-            curr.add(r.reference_end)
-            count += 1
+            if(len(curr) < maxCoverage):
+                curr.add(r.reference_end)
+                filtered += 1
 
     #TODO Write all reads still in curr to memory
 
-    print("Reduced BAM from " + bf.mapped + " to " + count + " reads")
-}
+    print("Reduced BAM from " + str(mapped) + " to " + str(filtered) + " reads")
 
 if __name__ == '__main__':
     main()
